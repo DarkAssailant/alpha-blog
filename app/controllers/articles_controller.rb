68ccbf5dc9369acf20a7310b2f25a9d0965034c8
Luis Diego P <lpd1211@gmail.com>
2016-06-28 00:08:50 +0000

    Added partials, delete, etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @article = Article.all
+  end
+
   def new
     @article = Article.new
   end
   
   def edit
      @article = Article.find(params[:id])
+  end
+  
+  def destroy
+     @article = Article.find(params[:id])
+     @article.destroy
+     flash[:notice] = "Article was succesfully deleted"
+     redirect_to articles_path
   end
   
   def create 
